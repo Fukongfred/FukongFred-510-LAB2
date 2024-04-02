d8c9b0fe78f31498309e07acaf40e45167afc1a6
Fukongfred <fucongxu@uw.edu>
2024-04-02 16:21:39 -0700

    init

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# FukongFred-510-LAB2
+# Techin-510-LAB2
+# Penguin Explorer
+
+A data analysis application to find out interesting insights about penguins in the Artic.
+
+## Get started
+```
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+streamlit run app.py
+```
+
+## Lessons Learned
+- **Filtering Data**: Users can filter the penguin data based on the range of bill lengths using a slider, select specific species using a dropdown menu, and choose one or more islands using a multiselect dropdown.
+- **Data Visualization**: The application visualizes the filtered penguin data using Plotly charts, including scatter plots and histograms, providing interactive visualizations for better understanding the data.
+
+## Questions
+- How to build a top bar that can be selected and the interface will navigate to the contents u selected?
+
+## TODO
+1. Link to your Streamlit cloud web app for data analysis and visualization
+2. A GitHub repo link
+    1. README.md
+    2. app.py
--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import plotly.express as px
 
 st.set_page_config(
     page_title="Penguins Explorer",
@@ -12,7 +13,40 @@ st.set_page_config(
 )
 
 st.title("🐧 Penguins Explorer")
+st.subheader('Introduction')
+about= '''
+A data analysis application to find out interesting insights about penguins in the Artic.
+'''
+st.markdown(about)
 
 df = pd.read_csv('https://raw.githubusercontent.com/mcnakhaee/palmerpenguins/master/palmerpenguins/data/penguins.csv')
 
+min_bill_length = df['bill_length_mm'].min()
+max_bill_length = df['bill_length_mm'].max()
+selected_bill_length = st.slider('Select Bill Length Range', min_bill_length, max_bill_length, (min_bill_length, max_bill_length))
+
+# Filter the dataframe based on the selected bill length range
+df = df[(df['bill_length_mm'] >= selected_bill_length[0]) & (df['bill_length_mm'] <= selected_bill_length[1])]
+
+species_filter=st.selectbox(
+    "Species", 
+    df["species"].unique(),
+    index=None)
+st.write(f"Selected Species: {species_filter}")
+if species_filter:
+    df = df[df['species'] == species_filter]
+
+islands_filter=st.multiselect('Island', df["island"].unique())
+if islands_filter:
+    df = df[df['island'].isin(islands_filter)]
+
 st.write(df)
+
+fig = px.scatter(df, x='bill_length_mm', y='bill_depth_mm', color='species', title='Penguin Bill Length vs Depth')
+st.plotly_chart(fig)
+
+fig2 = px.histogram(
+    df, 
+    x="bill_length_mm"
+)
+st.plotly_chart(fig2)
